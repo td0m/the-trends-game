7da25524bdb0e250f05e9c24a6cf22fe97967bf9
Dominik Tarnowski <dominik.t.uk@gmail.com>
2019-08-27 15:16:12 +0100

    add a home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
 
+import HomePage from "pages/HomePage";
 import RoomPage from "./pages/RoomPage";
 import RoomsPage from "pages/RoomsPage";
 
@@ -8,6 +9,7 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/" exact component={HomePage} />
         <Route path="/sign-in" exact component={RoomsPage} />
         <Route path="/rooms" exact component={RoomsPage} />
         <Route path="/rooms/:roomId" exact component={RoomPage} />
